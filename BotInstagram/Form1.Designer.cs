0c6b8dfdec48ba3156f4a24b5907a81de1221179
123@qwe <hashemkhajepour@yahoo.com>
2023-07-20 22:30:06 +0330

    login to instagram

---
+++ BotInstagram/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace BotInstagram
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbLogin = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.gbCommand = new System.Windows.Forms.GroupBox();
+            this.gbLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbLogin
+            // 
+            this.gbLogin.Controls.Add(this.btnLogin);
+            this.gbLogin.Controls.Add(this.txtPassword);
+            this.gbLogin.Controls.Add(this.label2);
+            this.gbLogin.Controls.Add(this.txtUserName);
+            this.gbLogin.Controls.Add(this.label1);
+            this.gbLogin.Location = new System.Drawing.Point(12, 13);
+            this.gbLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gbLogin.Name = "gbLogin";
+            this.gbLogin.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gbLogin.Size = new System.Drawing.Size(261, 238);
+            this.gbLogin.TabIndex = 0;
+            this.gbLogin.TabStop = false;
+            this.gbLogin.Text = "حساب کاربری :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username or Email :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password :";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(19, 68);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(234, 28);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(19, 141);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(234, 28);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(19, 192);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 38);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // gbCommand
+            // 
+            this.gbCommand.Enabled = false;
+            this.gbCommand.Location = new System.Drawing.Point(279, 13);
+            this.gbCommand.Name = "gbCommand";
+            this.gbCommand.Size = new System.Drawing.Size(579, 169);
+            this.gbCommand.TabIndex = 1;
+            this.gbCommand.TabStop = false;
+            this.gbCommand.Text = " دستورات :";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 737);
+            this.Controls.Add(this.gbCommand);
+            this.Controls.Add(this.gbLogin);
+            this.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ربات هوشمند";
+            this.gbLogin.ResumeLayout(false);
+            this.gbLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbCommand;
+    }
+}
+
---
+++ BotInstagram/Form1.cs
@@ -0,0 +1,45 @@
+﻿using InstagramApiSharp.API;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Logger;
+using System.Windows.Forms;
+
+namespace BotInstagram
+{
+    public partial class frmMain : Form
+    {
+        private static UserSessionData user;
+        private static IInstaApi api;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, System.EventArgs e)
+        {
+            user = new UserSessionData();
+            user.UserName = txtUserName.Text;
+            user.Password = txtPassword.Text;
+
+            api = InstaApiBuilder.CreateBuilder()
+                .SetUser(user)
+                .UseLogger(new DebugLogger(LogLevel.All))
+                .SetRequestDelay(RequestDelay.FromSeconds(0, 1))
+                .Build();
+
+            var loginRequest = await api.LoginAsync();
+
+            if(loginRequest.Succeeded)
+            {
+                gbLogin.Enabled = false;
+                gbCommand.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Login Field .... \n" + 
+                    loginRequest.Info.Message);
+            }
+        }
+    }
+}
